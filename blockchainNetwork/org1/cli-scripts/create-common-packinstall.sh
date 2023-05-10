7b61ab34e7d420420f34a76d6d07d7edd65cea15
k-farhana <farhana.kotal@gmail.com>
2023-05-11 00:45:22 +0530

    first commit

--- blockchainNetwork/org1/cli-scripts/create-common-packinstall.sh
+++ blockchainNetwork/org1/cli-scripts/create-common-packinstall.sh
@@ -2,27 +2,27 @@
 
 
 
-echo "************ Package Loan chaincode **********" 
-pushd /opt/gopath/src/github.com/chaincode/Loan/
+echo "************ Package Pineapple chaincode **********" 
+pushd /opt/gopath/src/github.com/chaincode/Pineapple/
 GO111MODULE=on go mod vendor
 popd
 
 CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.test.com/users/Admin@org1.test.com/msp CORE_PEER_ADDRESS=peer0.org1.test.com:7051 CORE_PEER_LOCALMSPID="org1MSP" CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.test.com/peers/peer0.org1.test.com/tls/ca.crt 
-peer lifecycle chaincode package Loan.tar.gz --path /opt/gopath/src/github.com/chaincode/Loan/ --lang golang --label Loan_1.0
+peer lifecycle chaincode package Pineapple.tar.gz --path /opt/gopath/src/github.com/chaincode/Pineapple/ --lang golang --label Pineapple_1.0
 
-echo "***************** Install Loan chaincode ***************"
+echo "***************** Install Pineapple chaincode ***************"
 CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.test.com/users/Admin@org1.test.com/msp CORE_PEER_ADDRESS=peer0.org1.test.com:7051 CORE_PEER_LOCALMSPID="org1MSP" CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.test.com/peers/peer0.org1.test.com/tls/ca.crt 
-peer lifecycle chaincode install Loan.tar.gz
+peer lifecycle chaincode install Pineapple.tar.gz
 
 
-echo "************ Package Shipment chaincode **********" 
-pushd /opt/gopath/src/github.com/chaincode/Shipment/
+echo "************ Package Jam chaincode **********" 
+pushd /opt/gopath/src/github.com/chaincode/Jam/
 GO111MODULE=on go mod vendor
 popd
 
 CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.test.com/users/Admin@org1.test.com/msp CORE_PEER_ADDRESS=peer0.org1.test.com:7051 CORE_PEER_LOCALMSPID="org1MSP" CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.test.com/peers/peer0.org1.test.com/tls/ca.crt 
-peer lifecycle chaincode package Shipment.tar.gz --path /opt/gopath/src/github.com/chaincode/Shipment/ --lang golang --label Shipment_1.0
+peer lifecycle chaincode package Jam.tar.gz --path /opt/gopath/src/github.com/chaincode/Jam/ --lang golang --label Jam_1.0
 
-echo "***************** Install Shipment chaincode ***************"
+echo "***************** Install Jam chaincode ***************"
 CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.test.com/users/Admin@org1.test.com/msp CORE_PEER_ADDRESS=peer0.org1.test.com:7051 CORE_PEER_LOCALMSPID="org1MSP" CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.test.com/peers/peer0.org1.test.com/tls/ca.crt 
-peer lifecycle chaincode install Shipment.tar.gz
+peer lifecycle chaincode install Jam.tar.gz
